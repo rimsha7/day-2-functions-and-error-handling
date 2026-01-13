ee65a167defa1ba0a148656c8a5ba0e71c3e4c02
rimsha7 <72002316+rimsha7@users.noreply.github.com>
2026-01-13 15:19:53 +0500

    initial commit

---
+++ errorhandling.py
@@ -0,0 +1,5 @@
+try:
+    x=int(input("enter a valid number: "))
+    print (10/x)
+except:
+    print("Error: Invalid number")
---
+++ function.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("Hello")
+def func_arg(name):
+    print("Hello",name)
+my_function()
+func_arg("Rimsha")
+def add(a,b):
+    return a+b
+result = add(7,20)
+print(result)
